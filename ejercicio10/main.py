700176bd984ab228f758f0ab3dd1415c8609aa9b
imjesusleal <98983618+imjesusleal@users.noreply.github.com>
2022-08-26 22:40:26 -0300

    Ejercicio sentencias de control v1

---
+++ ejercicio10/main.py
@@ -0,0 +1,7 @@
+edad_usuario = int(input("Ingresa tu edad "))
+if edad_usuario < 18:
+    print("Eres menor de edad")
+elif edad_usuario >= 18:
+    print("Eres mayor de edad")
+else: 
+    print("Lo que ingresaste no es un numero")
