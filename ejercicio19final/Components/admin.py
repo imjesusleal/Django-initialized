4617622272fe4d5714d3b40f86bfc8010ad3a935
imjesusleal <jesusalbertolv@gmail.com>
2022-09-21 13:47:44 -0300

    all changes

--- ejercicio19final/Components/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Directores, Peliculas
-
-admin.site.register(Directores)
-admin.site.register(Peliculas)
-
-# Register your models here.
--- ejercicio19final/Components/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='Index'),
-    path('<int:Directores_id>/', views.directores, name='directores'),
-    path('<int:Peliculas_id>/', views.peliculas, name = 'peliculas')
-]
